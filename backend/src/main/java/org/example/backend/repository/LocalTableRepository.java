1b31f9bc81efb609d52dece6039bd597830e00fd
antonio-carlosjr <profissional.contatocarlos@gmail.com>
2025-06-04 18:39:23 -0300

    feate- create table

--- backend/src/main/java/org/example/backend/repository/LocalTableRepository.java
+++ backend/src/main/java/org/example/backend/repository/LocalTableRepository.java
@@ -4,8 +4,11 @@ import org.example.backend.model.LocalTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LocalTableRepository extends JpaRepository<LocalTable, UUID> {
+
+    Optional<LocalTable> findByNumber(int number);
 }
