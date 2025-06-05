5e8fd7f976ab2b83c816b249a0eb9d11234dd1d2
allysson-assuncao <anybody723@proton.me>
2025-06-05 20:13:21 -0300

    adding the cross-origin annotation

--- backend/src/main/java/org/example/backend/controller/AuthController.java
+++ backend/src/main/java/org/example/backend/controller/AuthController.java
@@ -7,15 +7,13 @@ import org.example.backend.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/app/auth") // Base url, following the http://localhost:8080/
 public class AuthController {
 
