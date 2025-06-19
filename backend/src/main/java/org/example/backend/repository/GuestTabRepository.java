5a2c462b71734f7500a246bdce9338df23aab9dc
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2025-06-19 15:12:14 -0300

    Integração Produto - Pedido

--- backend/src/main/java/org/example/backend/repository/GuestTabRepository.java
+++ backend/src/main/java/org/example/backend/repository/GuestTabRepository.java
@@ -1,6 +1,7 @@
 package org.example.backend.repository;
 
 import org.example.backend.model.GuestTab;
+import org.example.backend.model.LocalTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
