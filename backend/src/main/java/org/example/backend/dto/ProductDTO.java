93321c354deea26ad8b4505c7e24f1dcab046862
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2025-06-22 17:19:42 -0300

    Alterando nome para id (categoria) ao registrar produto (ajuste)

--- backend/src/main/java/org/example/backend/dto/ProductDTO.java
+++ backend/src/main/java/org/example/backend/dto/ProductDTO.java
@@ -2,4 +2,4 @@ package org.example.backend.dto;
 
 import java.util.UUID;
 
-public record ProductDTO(String name, String description, double price, UUID Altera) { }
+public record ProductDTO(String name, String description, double price, UUID idCategory) { }
