7a9b7410778106b666dce20cc46aea27d3b15538
antonio-carlosjr <profissional.contatocarlos@gmail.com>
2025-06-27 16:38:19 -0300

    feat - making register table

---
+++ frontend/src/components/table/RegisterLocalTable.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function RegisterLocalTable (){
+
+    return (<div className="container w-full mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-6 text-center">Nova Mesa</h1>
+        <div>
+            <Add/>
+        </div>
+    </div>)
+}
