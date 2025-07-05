d3d32d07ec65e3cfae10606c8a704cf2404c409e
Rhuan Azevedo <rhuan2022@gmail.com>
2025-07-05 18:06:19 -0300

    feat: add verificação de comandas em aberto para uma mesa

--- backend/src/main/java/org/example/backend/repository/GuestTabRepository.java
+++ backend/src/main/java/org/example/backend/repository/GuestTabRepository.java
@@ -2,6 +2,7 @@ package org.example.backend.repository;
 
 import org.example.backend.model.GuestTab;
 import org.example.backend.model.LocalTable;
+import org.example.backend.model.enums.GuestTabStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,5 @@ import java.util.UUID;
 public interface GuestTabRepository extends JpaRepository<GuestTab, Long>, JpaSpecificationExecutor<GuestTab> {
     List<GuestTab> findByLocalTable(LocalTable localTable);
     List<GuestTab> findByLocalTableId(UUID localTableId);
+    boolean existsByLocalTable_NumberAndStatus(int number, GuestTabStatus status);
 }
