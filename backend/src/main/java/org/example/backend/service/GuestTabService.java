863fbc5d8d45613b00114cea65f23d7853d065c4
Kevin Rehem <kevin.rehem@gmail.com>
2025-07-11 21:45:07 -0300

    (FEAT) getGuestTabsByTableNumber_WhenTableNumberExists_ShouldReturnGuestTabGetDTOs [TESTED/WORKING]

--- backend/src/main/java/org/example/backend/service/GuestTabService.java
+++ backend/src/main/java/org/example/backend/service/GuestTabService.java
@@ -111,7 +111,7 @@ public class GuestTabService {
         return result;
     }
 
-    public GuestTabGetDTO convertGuestTabToGetDTO(GuestTab guestTab) {
+    private GuestTabGetDTO convertGuestTabToGetDTO(GuestTab guestTab) {
         return GuestTabGetDTO.builder()
                 .id(guestTab.getId())
                 .tableNumber(guestTab.getLocalTable().getNumber())
