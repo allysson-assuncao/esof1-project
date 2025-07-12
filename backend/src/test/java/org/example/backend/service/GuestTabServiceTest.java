79225ff32e8f097cf4a0d5788f0303ba0a520a51
Kevin Rehem <kevin.rehem@gmail.com>
2025-07-11 21:16:31 -0300

    (FEAT) getGuestTabsByTableNumber_WhenTableNumberExists_ShouldReturnGuestTabGetDTOs [TESTED/WORKING]

--- backend/src/test/java/org/example/backend/service/GuestTabServiceTest.java
+++ backend/src/test/java/org/example/backend/service/GuestTabServiceTest.java
@@ -2,7 +2,9 @@ package org.example.backend.service;
 
 import org.example.backend.dto.GuestTab.GuestTabGetDTO;
 import org.example.backend.dto.GuestTab.GuestTabRequestDTO;
+import org.example.backend.model.GuestTab;
 import org.example.backend.model.LocalTable;
+import org.example.backend.model.enums.GuestTabStatus;
 import org.example.backend.repository.GuestTabRepository;
 import org.example.backend.repository.LocalTableRepository;
 import org.junit.jupiter.api.Test;
@@ -12,8 +14,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -67,6 +71,34 @@ public class GuestTabServiceTest {
         // verificação
         assertNotNull(result, "Resultado retornou null");
         assertArrayEquals(new GuestTabGetDTO[]{}, result.toArray(), "Lista de mesa inexistente veio preenchida");
+
+    }
+
+    @Test
+    void getGuestTabsByTableNumber_WhenTableNumberExists_ShouldReturnGuestTabGetDTOs(){
+        // config mock
+        LocalTable mockLocalTable = LocalTable.builder()
+                .number(new Random().nextInt(1000)+1)
+                .build();
+        List<GuestTab> mockGuestTabs = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            GuestTab mockGuestTab = GuestTab.builder()
+                    .localTable(mockLocalTable)
+                    .status(GuestTabStatus.OPEN)
+                    .build();
+            mockGuestTabs.add(mockGuestTab);
+        }
+        when(localTableRepository.findByNumber(mockLocalTable.getNumber()))
+                .thenReturn(Optional.of(mockLocalTable));
+        when(guestTabRepository.findByLocalTable(mockLocalTable)).thenReturn(mockGuestTabs);
+
+        // execução
+        List<GuestTabGetDTO> result = guestTabService.getGuestTabsByTableNumber(mockLocalTable.getNumber());
+
+        // verificação
+        assertNotNull(result, "Resultado retornou null");
+        assertNotEquals(new ArrayList<>(), result, "Retornou lista vazia");
+        assertEquals(GuestTabGetDTO.class, result.getFirst().getClass(), "");
 
     }
 
