424a47f774449e19d5d1544a62e2594b3bf77220
allysson-assuncao <anybody723@proton.me>
2025-07-26 07:12:30 -0300

    fixing the payment status exhibition

--- frontend/src/components/form/add/AddOrderForm.tsx
+++ frontend/src/components/form/add/AddOrderForm.tsx
@@ -30,8 +30,6 @@ export function AddOrderForm({guestTabId, parentOrderId, onSuccess}: AddOrderFor
         () => fetchSimpleProductsIfAdditional(parentOrderId? parentOrderId : -1),
     );
 
-
-
     const form = useForm<RegisterOrdersFormData>({
         resolver: zodResolver(registerOrdersFormSchema),
         defaultValues: {
--- frontend/src/components/report/sub-table/PaymentsSubTable.tsx
+++ frontend/src/components/report/sub-table/PaymentsSubTable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ReportPayment} from "@/model/Interfaces";
+import {PaymentStatus, ReportPayment} from "@/model/Interfaces";
 import {ColumnDef, flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
 import {getExpandedRowModel} from "@tanstack/table-core";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
@@ -7,14 +7,31 @@ import {IndividualPaymentsSubTable} from "@/components/report/sub-table/Individu
 import {ChevronDown, ChevronRight} from "lucide-react";
 
 const reportPaymentColumns = (): ColumnDef<ReportPayment>[] => [
-    { id: "expander", cell: ({ row }) => <button onClick={() => row.toggleExpanded(!row.getIsExpanded())}>{row.getIsExpanded() ? <ChevronDown /> : <ChevronRight />}</button> },
-    { accessorKey: "id", header: "ID Pag." },
-    { accessorKey: "status", header: "Status" },
-    { accessorKey: "totalAmount", header: "Valor", cell: ({ row }) => `R$ ${row.original.totalAmount.toFixed(2)}` },
-    { accessorKey: "createdAt", header: "Hora", cell: ({ row }) => new Date(row.original.createdAt).toLocaleTimeString('pt-BR') },
+    {
+        id: "expander",
+        cell: ({row}) => <button onClick={() => row.toggleExpanded(!row.getIsExpanded())}>{row.getIsExpanded() ?
+            <ChevronDown/> : <ChevronRight/>}</button>
+    },
+    {accessorKey: "id", header: "ID Pag."},
+    {
+        accessorKey: "status", header: "Status", cell: ({row}) => {
+            return PaymentStatus[row.original.status as keyof typeof PaymentStatus]?.label || row.original.status || "-";
+        },
+    },
+    {accessorKey: "totalAmount", header: "Valor", cell: ({row}) => `R$ ${row.original.totalAmount.toFixed(2)}`},
+    {
+        accessorKey: "createdAt",
+        header: "Hora",
+        cell: ({row}) => new Date(row.original.createdAt).toLocaleTimeString('pt-BR')
+    },
+    {
+        accessorKey: "updatedAt",
+        header: "Data",
+        cell: ({row}) => new Date(row.original.createdAt).toLocaleDateString('pt-BR')
+    },
 ];
 
-export const PaymentsSubTable = ({ payments }: { payments: ReportPayment[] }) => {
+export const PaymentsSubTable = ({payments}: { payments: ReportPayment[] }) => {
     const columns = React.useMemo(() => reportPaymentColumns(), []);
     const table = useReactTable({
         data: payments,
@@ -30,7 +47,8 @@ export const PaymentsSubTable = ({ payments }: { payments: ReportPayment[] }) =>
                     {table.getHeaderGroups().map(headerGroup => (
                         <TableRow key={headerGroup.id}>
                             {headerGroup.headers.map(header => (
-                                <TableHead key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+                                <TableHead
+                                    key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
                             ))}
                         </TableRow>
                     ))}
@@ -40,13 +58,15 @@ export const PaymentsSubTable = ({ payments }: { payments: ReportPayment[] }) =>
                         <React.Fragment key={row.id}>
                             <TableRow>
                                 {row.getVisibleCells().map(cell => (
-                                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                                    <TableCell
+                                        key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
                                 ))}
                             </TableRow>
                             {row.getIsExpanded() && (
                                 <TableRow>
                                     <TableCell colSpan={columns.length} className="p-0">
-                                        <IndividualPaymentsSubTable individualPayments={row.original.individualPayments} />
+                                        <IndividualPaymentsSubTable
+                                            individualPayments={row.original.individualPayments}/>
                                     </TableCell>
                                 </TableRow>
                             )}
