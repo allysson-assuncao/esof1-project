74c83e3c8462773fb388d6dc12807236f5c188f0
Kevin Rehem <kevin.rehem@gmail.com>
2025-08-06 13:23:07 -0300

    ~(FEAT) PaymentTests [WIP]

--- backend/src/test/java/org/example/backend/service/PaymentServiceTest.java
+++ backend/src/test/java/org/example/backend/service/PaymentServiceTest.java
@@ -1,7 +1,9 @@
 package org.example.backend.service;
 
+import org.example.backend.dto.IndividualPayment.SimpleIndividualPaymentDTO;
 import org.example.backend.dto.Payment.RegisterPaymentRequestDTO;
-import org.example.backend.model.Payment;
+import org.example.backend.model.*;
+import org.example.backend.model.enums.PaymentStatus;
 import org.example.backend.repository.PaymentMethodRepository;
 import org.example.backend.repository.PaymentRepository;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,6 +50,29 @@ public class PaymentServiceTest {
         // config mock
         Long paymentId = new Random().nextLong();
         RegisterPaymentRequestDTO paymentRequestDTO = Mockito.mock(RegisterPaymentRequestDTO.class);
+        when(paymentRepository.findById(paymentId)).thenReturn(Optional.of(Payment.builder()
+                .status(PaymentStatus.PAID).build()));
+
+        // execução e teste
+        assertThrows(IllegalStateException.class, () -> paymentService.registerIndividualPayments(paymentId, paymentRequestDTO));
+    }
+
+    @Test
+    void registerIndividualPayment_WhenValueIsGreaterThanTabValue_ShouldThrowIllegalStateException() {
+        // config mock
+        Long paymentId = new Random().nextLong();
+        List<SimpleIndividualPaymentDTO> individualPayments = new ArrayList<>();
+        individualPayments.add(SimpleIndividualPaymentDTO.builder().amount(BigDecimal.valueOf(212.00)).build());
+        RegisterPaymentRequestDTO paymentRequestDTO = new RegisterPaymentRequestDTO(individualPayments);
+        Product product = Product.builder().price(20.00).build();
+        List<Order> orders = new ArrayList<>();
+        orders.add(Order.builder().amount(2).product(product).build());
+        GuestTab guestTab = GuestTab.builder().orders(orders).build();
+        when(paymentRepository.findById(paymentId)).thenReturn(Optional.of(Payment.builder().status(PaymentStatus.PENDING).totalAmount(BigDecimal.valueOf(20.00))
+                .guestTab(guestTab).build()));
+
+        // execução e teste
+        assertThrows(IllegalStateException.class, () -> paymentService.registerIndividualPayments(paymentId,paymentRequestDTO));
     }
 
 }
